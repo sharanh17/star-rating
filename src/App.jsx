45da32efe7ba6683e475434530d59b2fb589bea9
sharan <sharanhidaduggi17@gmail.com>
2024-11-20 15:37:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import StarRating from "./components/star";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <StarRating noOfStars={10} />
+    </>
+  );
+}
+
+export default App;
